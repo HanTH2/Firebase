498f4e00efdd4619921ec6017bb1c2bd27b6ec26
oNguyenThanhTuan <NgoThanh.Tuan@access-company.com>
2016-08-16 13:15:59 +0700

    update time

--- AppChat/app/src/main/java/com/hanth2/appchat/utils/ConvertTime.java
+++ AppChat/app/src/main/java/com/hanth2/appchat/utils/ConvertTime.java
@@ -1,18 +1,30 @@
 package com.hanth2.appchat.utils;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by zero on 15/08/2016.
  */
 public class ConvertTime {
     public static String convertTimestamp(long time) {
-        Date date = new Date(time);
-        String result = new SimpleDateFormat("HH:mm").format(date);
-        if (result.indexOf(0) == 0) {
-            result = result.substring(1, result.length());
-        }
-        return result;
+//        Date date = new Date(time);
+//        String result = new SimpleDateFormat("HH:mm").format(date);
+//        if (result.indexOf(0) == 0) {
+//            result = result.substring(1, result.length());
+//        }
+//        return result;
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+7:00"));
+        Date currentLocalTime = cal.getTime();
+        DateFormat date = new SimpleDateFormat("HH:mm a");
+        // you can get seconds by adding  "...:ss" to it
+        date.setTimeZone(TimeZone.getTimeZone("GMT+7:00"));
+
+        String localTime = date.format(currentLocalTime);
+
+        return localTime;
     }
 }
